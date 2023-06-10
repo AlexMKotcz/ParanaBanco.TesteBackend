8521f1b610a489a1043c80f6e8466db143a4aae6
Alex <alexmarianokotcz@outlook.com.br>
2023-06-10 17:30:05 -0300

    Adding IRepository interfaces

---
+++ ParanaBanco.TesteBackend.Domain/interfaces/irepository/IClientRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+using ParanaBanco.TesteBackend.Domain.Entities;
+
+namespace ParanaBanco.TesteBackend.Domain.Interfaces.IRepository;
+public interface IClientRepository
+{
+    Task AddAsync(Client client);
+    IQueryable<int> Count(Expression<Func<Client, bool>>? expression = null);
+
+    IQueryable<Client> Get(Expression<Func<Client, bool>>? expression = null);
+
+    Task UpdateAsync(Client client);
+}
---
+++ ParanaBanco.TesteBackend.Domain/interfaces/irepository/IPhoneRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+using ParanaBanco.TesteBackend.Domain.Entities;
+
+namespace ParanaBanco.TesteBackend.Domain.Interfaces.IRepository;
+public interface IPhoneRepository
+{
+    Task AddAsync(Phone phone);
+    IQueryable<int> Count(Expression<Func<Phone, bool>>? expression = null);
+
+    IQueryable<Phone> Get(Expression<Func<Phone, bool>>? expression = null);
+
+    Task UpdateAsync(Phone client);
+}
