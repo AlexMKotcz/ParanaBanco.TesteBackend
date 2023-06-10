08467d5a249707e86b94a2a636226a876b2435dc
Alex Mariano <alex.mariano@telemetrix.com.br>
2023-06-09 22:49:54 -0300

    Adding Unit Test

--- ParanaBanco.TesteBackend.Domain.Tests/ClientUnitTest1.cs
+++ ParanaBanco.TesteBackend.Domain.Tests/ClientUnitTest1.cs
@@ -11,8 +11,11 @@ public class ClientUnitTest1
     [Fact]
     public void CreateClient_WithValidParameters_ResultObjectValidState()
     {
-        Action action = () => new Client("Alex Mariano Kotcz", "alexmariano129@gmail.com",
-            new List<Phone>() { new Phone(41, "988257783", Enums.EPhoneType.Mobile) });
+        Action action = () =>
+        {
+            _ = new Client("Alex Mariano Kotcz", "alexmariano129@gmail.com",
+                        new List<Phone>() { new Phone(41, "988257783", Enums.EPhoneType.Mobile) });
+        };
 
         action.Should().NotThrow<DomainExceptionValidation>();
     }
