4353f8906c4c380882742739e3fb527e0ae760c3
Alex Mariano <alex.mariano@telemetrix.com.br>
2023-06-09 22:36:36 -0300

    Adding test project

---
+++ ParanaBanco.TesteBackend.Domain.Tests/ClientUnitTest1.cs
@@ -0,0 +1,24 @@
+﻿
+using FluentAssertions;
+
+using ParanaBanco.TesteBackend.Domain.Entities;
+using ParanaBanco.TesteBackend.Domain.Validation;
+
+using Xunit;
+namespace ParanaBanco.TesteBackend.Domain.Tests;
+public class ClientUnitTest1
+{
+    [Fact]
+    public void CreateClient_WithValidParameters_ResultObjectValidState()
+    {
+        Action action = () => new Client("Alex Mariano Kotcz", "alexmariano129@gmail.com",
+            new List<Phone>() { new Phone(41, "988257783", Enums.EPhoneType.Mobile) });
+
+        action.Should().NotThrow<DomainExceptionValidation>();
+    }
+}
+
+public class PhoneUnitTest1
+{
+
+}
