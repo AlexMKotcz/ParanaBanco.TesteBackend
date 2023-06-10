5ab29b04ad14b950262d7def5afff6724b225193
Alex <alexmarianokotcz@outlook.com.br>
2023-06-10 15:32:54 -0300

    UnitTest Segregation

---
+++ ParanaBanco.TesteBackend.Domain.Tests/PhoneUnitTest1.cs
@@ -0,0 +1,19 @@
+﻿
+using FluentAssertions;
+
+using ParanaBanco.TesteBackend.Domain.Entities;
+using ParanaBanco.TesteBackend.Domain.Validation;
+
+using Xunit;
+namespace ParanaBanco.TesteBackend.Domain.Tests;
+
+public class PhoneUnitTest1
+{
+    [Fact]
+    public void CreatePhone_WithValidParameters_ResultObjectValidState()
+    {
+        Action action = () => { _ = new Phone(41, "988257783", Enums.EPhoneType.LandLine); };
+
+        action.Should().NotThrow<DomainExceptionValidation>();
+    }
+}
