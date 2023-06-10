22528d9165d2e5a0cda40f5e4342fb5d96b5d9d1
Alex <alexmarianokotcz@outlook.com.br>
2023-06-10 18:13:40 -0300

    Adding controllers

---
+++ ParanaBanco.TesteBackend.API/Controllers/ClientController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ParanaBanco.TesteBackend.Application.Interfaces;
+
+namespace ParanaBanco.TesteBackend.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ClientController : ControllerBase
+{
+    private readonly IClientService _clientService;
+
+    public ClientController(IClientService clientService)
+    {
+        _clientService = clientService;
+    }
+}
---
+++ ParanaBanco.TesteBackend.API/Controllers/PhoneController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParanaBanco.TesteBackend.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class PhoneController : ControllerBase
+{
+
+}
