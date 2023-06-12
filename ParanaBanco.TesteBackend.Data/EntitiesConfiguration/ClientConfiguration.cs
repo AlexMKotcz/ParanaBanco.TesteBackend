c6a1789f534c6509ca08df383c60623f996d055c
Alex <alexmarianokotcz@outlook.com.br>
2023-06-11 22:17:19 -0300

    Adding entitiesConfiguration

---
+++ ParanaBanco.TesteBackend.Data/EntitiesConfiguration/ClientConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using ParanaBanco.TesteBackend.Domain.Entities;
+
+namespace ParanaBanco.TesteBackend.Data.EntitiesConfiguration;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> entity)
+    {
+        entity.HasKey(c => c.Id);
+        entity.Property(c => c.FullName).IsRequired();
+        entity.Property(c => c.Email).IsRequired();
+
+        entity.HasMany(c => c.Phones)
+            .WithOne(p => p.Client)
+            .HasForeignKey(p => p.ClientId);
+    }
+}
---
+++ ParanaBanco.TesteBackend.Data/EntitiesConfiguration/PhoneConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using ParanaBanco.TesteBackend.Domain.Entities;
+
+namespace ParanaBanco.TesteBackend.Data.EntitiesConfiguration;
+
+public class PhoneConfiguration : IEntityTypeConfiguration<Phone>
+{
+    public void Configure(EntityTypeBuilder<Phone> entity)
+    {
+        entity.HasKey(p => p.Id);
+        entity.Property(p => p.DDD).IsRequired();
+        entity.Property(p => p.Number).IsRequired().HasMaxLength(9);
+        entity.Property(p => p.Type).IsRequired();
+    }
+}
