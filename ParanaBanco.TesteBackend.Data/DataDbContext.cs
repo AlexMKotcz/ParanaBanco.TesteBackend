cc0eeb734205fc15a96bb3be9fa3cbb55f479a66
Alex <alexmarianokotcz@outlook.com.br>
2023-06-11 23:10:02 -0300

    Fix: DbContext CTORs (configuration)

--- ParanaBanco.TesteBackend.Data/DataDbContext.cs
+++ ParanaBanco.TesteBackend.Data/DataDbContext.cs
@@ -5,12 +5,16 @@ using ParanaBanco.TesteBackend.Domain.Entities;
 namespace ParanaBanco.TesteBackend.Data;
 public class DataDbContext : DbContext
 {
-    public DbSet<Client> Clients { get; set; }
-    public DbSet<Phone> Phones { get; set; }
+    public DataDbContext() { }
+    public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(DataDbContext).Assembly);
     }
+
+
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Phone> Phones { get; set; }
 }
