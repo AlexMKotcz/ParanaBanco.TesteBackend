e4dc471894ca81ca7d63ff5b95b9935b8870925a
Alex <alexmarianokotcz@outlook.com.br>
2023-06-11 22:17:30 -0300

    Adding the dbContext

--- ParanaBanco.TesteBackend.Data/DataDbContext.cs
+++ ParanaBanco.TesteBackend.Data/DataDbContext.cs
@@ -1,7 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+using ParanaBanco.TesteBackend.Domain.Entities;
+
 namespace ParanaBanco.TesteBackend.Data;
 public class DataDbContext : DbContext
 {
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<Phone> Phones { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.ApplyConfigurationsFromAssembly(typeof(DataDbContext).Assembly);
+    }
 }
